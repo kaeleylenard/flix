3bd8a151c0de20637736253db4e55ee5e93269fe
= <lenardk@uci.edu>
2020-01-19 13:46:25 -0800

    Customized cells to format movie poster, title, and synopsis.

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -50,12 +50,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
         
-        cell.textLabel!.text = title
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath) 
+        
         return cell
     }
     
