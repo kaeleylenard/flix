57762f58736bee7116fce9ed1907d852979f2420
= <lenardk@uci.edu>
2020-01-26 19:20:08 -0800

    Collection View Setup

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kaeley Lenard on 1/26/20.
+//  Copyright © 2020 kaeleylenard. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
